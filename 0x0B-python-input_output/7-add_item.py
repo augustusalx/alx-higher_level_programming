452cc41865c482ea5f12933b029edc99efe37da7
augustusalx <augustusemmanuel91@gmail.com>
2023-02-26 00:43:31 -0800

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""Add item script."""
-import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""A module for save_to_json_file."""
 
-try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+import json
 
-argc = len(sys.argv)
 
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file, using a JSON representation."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(my_obj))
